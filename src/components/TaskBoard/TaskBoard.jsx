38f7fe458c5504844e7a7307ae600d5aedde2650
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-05-03 23:37:52 +0300

    TaskBoard-JS-implement

--- src/components/TaskBoard/TaskBoard.jsx
+++ src/components/TaskBoard/TaskBoard.jsx
@@ -1,10 +1,22 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, { useState } from "react";
 import SearchTask from "./SearchTask";
 import ActionTask from "./ActionTask";
 import ListTask from "./ListTask";
 
+const defaultTask = {
+  id: crypto.randomUUID(),
+  title: "Learn React",
+  description:
+    " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque voluptatem ea nihil laudantium doloremque itaque, eos, similique nostrum porro libero quas necessitatibus consequatur natus neque iste consectetur accusantium ab impedit!",
+  tags: ["web", "reactJS", "javascript"],
+  priority: "High",
+  isFavorite: false,
+};
+
 const TaskBoard = () => {
+  const [tasks, setTasks] = useState([defaultTask]);
+
   return (
     <div className=" w9/12 mx-auto">
       <section className="mb-20" id="tasks">
@@ -15,7 +27,8 @@ const TaskBoard = () => {
 
           <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
             <ActionTask></ActionTask>
-            <ListTask></ListTask>
+
+            <ListTask tasks={tasks}></ListTask>
           </div>
         </div>
       </section>
